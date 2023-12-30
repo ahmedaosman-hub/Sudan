8b3c9a311eb14a085a29b59b85dfcdf160e06adc
Ahmed Osman <ahmedaosman00@gmail.com>
2023-12-30 13:12:35 -0800

    Refactor slide-out menu

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const openMenuButton = document.getElementById("openMenu");
-  const closeMenuButton = document.getElementById("closeMenu");
-  const navbarDropdown = document.getElementById("navbarDropdown");
+  // JavaScript to toggle the slide-out menu
+  const menuButton = document.querySelector(".navbar-toggle");
+  const slideOutMenu = document.querySelector(".slide-out-menu");
+  const overlay = document.querySelector(".overlay");
 
-  // Function to show the dropdown
-  function showDropdown() {
-    navbarDropdown.style.display = "block"; // or 'flex' if you are using Flexbox in the dropdown
-    closeMenuButton.style.display = "block"; // Show the close button
-    openMenuButton.style.display = "none"; // Hide the hamburger button
-    console.log("Dropdown is shown");
-  }
+  menuButton.addEventListener("click", () => {
+    slideOutMenu.style.left = slideOutMenu.style.left === "0%" ? "-100%" : "0%";
+    overlay.style.display =
+      overlay.style.display === "block" ? "none" : "block";
+  });
 
-  // Function to hide the dropdown
-  function hideDropdown() {
-    navbarDropdown.style.display = "none";
-    closeMenuButton.style.display = "none"; // Hide the close button
-    openMenuButton.style.display = "block"; // Show the hamburger button
-    console.log("Dropdown is hidden");
-  }
+  // Close the menu when clicking on the overlay
+  overlay.addEventListener("click", () => {
+    slideOutMenu.style.left = "-100%";
+    overlay.style.display = "none";
+  });
+});
 
-  // Event listener for the open button
-  openMenuButton.addEventListener("click", showDropdown);
+// Add this script at the end of your HTML, before the closing </body> tag.
+document.addEventListener("DOMContentLoaded", function () {
+  var menuToggle = document.querySelector(".navbar-toggle");
+  var slideOutMenu = document.getElementById("slide-out-menu");
 
-  // Event listener for the close button
-  closeMenuButton.addEventListener("click", hideDropdown);
+  menuToggle.addEventListener("click", function () {
+    this.children[0].classList.toggle("active");
+    slideOutMenu.classList.toggle("active");
+  });
+
+  // Since you're not using the overlay, there's no need to close the menu when it's clicked
 });
